ea7947b2baf8fd76d46b70aeaa77ba4f4afb5329
Muradyan David <muradyan98@list.ru>
2025-06-27 01:13:51 +0300

    Edit informations

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import ScrollToTop from "./utiles/ScrollToTop";
 export default function App() {
   return (
     <div className="App">
-        <Router>
+        <Router basename="/">
           <ScrollToTop />
           <Navbar />
           <Routes>
